7a9483be8b222640aa23fa0f1fd19adae8516390
liangrq <liangrq@efly.cc>
2022-09-29 16:11:27 +0800

    11

--- control/bgp_control.py
+++ control/bgp_control.py
@@ -5,7 +5,7 @@ from bgp_vrf import *
 import sys
 import bgp_control as this
 
-def find_peer(none):
+def find_peer(**args):
     auto_discover_peer()
 
 def add_del_path(bgp_as, prefix, prefix_len, vrf_id, is_add):
@@ -13,10 +13,8 @@ def add_del_path(bgp_as, prefix, prefix_len, vrf_id, is_add):
         add_path(bgp_as, prefix, prefix_len, vrf_id)
 
 if __name__ == '__main__':
-    print(sys.argv)
-    exit()
     if hasattr(this, sys.argv[1]):
-        getattr(this, sys.argv[1])(sys.argv[2:])
+        getattr(this, sys.argv[1])(*(sys.argv[2:]))
     else:
         print('Usage:')
 
