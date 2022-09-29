3c4a0bd709a5500c40cbb16cf26dc0b6486970fa
liangrq <liangrq@efly.cc>
2022-09-29 16:05:38 +0800

    11

--- control/bgp_control.py
+++ control/bgp_control.py
@@ -13,6 +13,8 @@ def add_del_path(bgp_as, prefix, prefix_len, vrf_id, is_add):
         add_path(bgp_as, prefix, prefix_len, vrf_id)
 
 if __name__ == '__main__':
+    print(sys.argv)
+    exit()
     if hasattr(this, sys.argv[1]):
         getattr(this, sys.argv[1])(sys.argv[2:])
     else:
